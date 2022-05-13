d7bb082171f36dcbf5b53c1e3a0da06b2330b059
Jonathan Dumlao <dumlao.jonathan.g.5165@gmail.com>
2022-05-13 10:21:46 +0800

    update

--- js/script.js
+++ js/script.js
@@ -1,13 +1,16 @@
 $("document").ready(function(){
     
-    $(".menuicon").click(function(){
-        $(".menuicon").css("transform","scale(1.2)");
-        $(".nav").slideToggle();
-    
+    $(".menuicon").click(500,function(){
+        $(".menuicon").fadeToggle(100, function(){
+            $(".nav").slideToggle();
+
+        });      
     })
 
     $(".nav").click(function(){
         $(this).slideToggle();
         $(".menuicon").css("transform","scale(1)");
+        $(".menuicon").toggle();
     });
 })
+
