bcd52463a5bd6a2e61a6aaa182ef07cfdcd57e0d
Jonathan Dumlao <dumlao.jonathan.g.5165@gmail.com>
2022-05-13 13:00:35 +0800

    updated: added navigation slide animation

--- js/script.js
+++ js/script.js
@@ -2,13 +2,14 @@ $("document").ready(function(){
     
     $(".menuicon").click(500,function(){
         $(".menuicon").fadeToggle(100, function(){
-            $(".nav").slideToggle();
+            $(".nav").toggle("slide", {direction:'right'});
+            
 
         });      
     })
 
     $(".nav").click(500, function(){
-        $(this).slideToggle(500, function(){
+        $(this).toggle("slide",{direction:"right"},500, function(){
             $(".menuicon").fadeToggle();
         });
     });
